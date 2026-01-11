51e155055d231f129014a2cc10b881431ea40d96
Fabriconcik <fabriconcik@gmail.com>
2026-01-11 18:23:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1160,9 +1160,11 @@ class EmojiBattleGame:
             verdicts = {}
             for part in parts:
                 part_player = part.split('\n')
-                player = part_player[0].replace("Игрок:", '').strip()
-                player = player.replace("игрок:", '').strip()
+                
                 player = part_player[0].replace(":", '').strip()
+                player = part_player[0].replace("Игрок", '').strip()
+                player = player.replace("игрок", '').strip()
+
                 score = part_player[1].replace(":", '').strip()
                 score = part_player[1].replace("/10", '').strip()
                 verdicts[player] = score
